b454ca0fbfef03a5be103e327be5b63a0b34f51e
Nicolae Sircu <sircuniku1@gmail.com>
2025-02-26 16:04:26 +0200

    Refactor MainLayout to include Sidebar and update App component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
-import Sidebar from "./components/Sidebar";
 import MainLayout from "./layouts/MainLayout";
 
 export default function App() {
-  return (
-    <MainLayout>
-      <Sidebar />
-    </MainLayout>
-  );
+  return <MainLayout>Content</MainLayout>;
 }
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import Sidebar from "../components/Sidebar";
 
 interface Props {
   children: ReactNode;
@@ -7,5 +8,11 @@ interface Props {
 export default function MainLayout(props: Props) {
   const { children } = props;
 
-  return <div className="flex h-screen">{children}</div>;
+  return (
+    <div className="flex h-screen">
+      <Sidebar />
+
+      <main className="flex-1">{children}</main>
+    </div>
+  );
 }
